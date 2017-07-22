ba272e353423a10bd0ee28dd852ee59449b10d28
Samuel Richardson <samrichardson@sbcglobal.net>
2017-07-22 16:56:02 -0400

    initial commit

---
+++ TarkSharkBot.py
@@ -0,0 +1,30 @@
+import os
+import json
+
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def webhook():
+    data = request.get_json()
+
+    #ensure message is not from self
+    if data['name'] != 'TarkShark':
+        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+        send_message(msg)
+
+    return "ok", 200
+
+ef send_message(msg):
+  url  = 'https://api.groupme.com/v3/bots/post'
+
+  data = {
+          'bot_id' : os.getenv('TARKSHARK_BOT_ID'),
+          'text'   : msg,
+         }
+  request = Request(url, urlencode(data).encode())
+  json = urlopen(request).read().decode()
