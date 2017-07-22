f94805feaa352fa004b102ccc6abc3d73cb2d544
Samuel Richardson <samrichardson@sbcglobal.net>
2017-07-22 18:37:48 -0400

    commit

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ from urllib.request import Request, urlopen
 from flask import Flask, request
 
 app = Flask(__name__)
-app.config.update(PROPAGATE_EXCEPTIONS=True)
-@app.route('/', methods=['POST'])
+@app.route('/post',methods=['POST'])
 def webhook():
   data = request.get_json()
   log('Recieved {}'.format(data))
