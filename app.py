c00da631fc932bc49a13c8cc3627aa14d2d346c4
Samuel Richardson <samrichardson@sbcglobal.net>
2017-07-22 18:12:45 -0400

    change flask to output more error

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from urllib.request import Request, urlopen
 from flask import Flask, request
 
 app = Flask(__name__)
-
+app.config.update(PROPAGATE_EXCEPTIONS=True)
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
