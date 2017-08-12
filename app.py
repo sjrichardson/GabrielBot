d4ea1d3f3a6e87642f136121355cbd5b8dd47907
Samuel Richardson <samrichardson@sbcglobal.net>
2017-08-12 13:27:55 -0400

    w

--- app.py
+++ app.py
@@ -7,5 +7,4 @@ import requests
 
 postUrl = 'https://api.groupme.com/v3/bots/post'
 r = requests.get('tarksharkbot.herokuapp.com/')
-if r.status_code == 200
-    q = request.post(postUrl, data = {'text': 'yay', 'bot_id' : 'd6981906b891bb32c944c96fd3'})
+q = request.post(postUrl, data = {'text': r.status_code, 'bot_id' : 'd6981906b891bb32c944c96fd3'})
