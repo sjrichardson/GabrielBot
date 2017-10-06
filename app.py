bb1e99ea5c1ecb7498c35ed78f7e002ac6762d0c
Samuel Richardson <samrichardson@sbcglobal.net>
2017-10-06 13:55:10 -0400

    write webhook and send function

--- app.py
+++ app.py
@@ -10,4 +10,16 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
+    if data['name'] != 'tarksharkbot':
+        msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+        send_message(msg)
     return "ok", 200
+def send_message(msg):
+    url = 'https://api.groupme.com/v3/bots/post'
+
+    data = {
+        'bot_id' : os.getenv('GROUPME_BOT_ID'),
+        'text' : msg,
+    }
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
