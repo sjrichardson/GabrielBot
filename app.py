4e2dc9bb1e2d349675e43838ed57dcae5ff82913
Samuel Richardson <samrichardson@sbcglobal.net>
2017-10-06 18:24:47 -0400

    hello' '

--- app.py
+++ app.py
@@ -35,5 +35,5 @@ def bible_search(reference):
     bible_url = "https://bibles.org/v2/passages.js"
     res = requests.get(bible_url, auth=(biblekey, 'X'), params=payload)
     response = res.json()
-    passage = response['response']['search']['result']['passages']
-    print (passage['text'])
+    passage = response['response']['search']['result']['passages']['text']
+    print (dumps(passage))
