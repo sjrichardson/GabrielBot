3d001f96b3fbc572559ac55c130c9c41153373c7
Samuel Richardson <samrichardson@sbcglobal.net>
2017-10-06 16:33:28 -0400

    plz bb

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
 import sys
-import json
 
 from urllib.parse import urlencode
-from urllib.request import Request, urlopen
-
+import requests
+from json import dumps
 
 from flask import Flask, request
 
@@ -17,13 +16,11 @@ def webhook():
         send_message(msg)
     return "ok", 200
 def send_message(msg):
-    url = 'https://api.groupme.com/v3/bots/post'
-    data = {
+    send_url = 'https://api.groupme.com/v3/bots/post'
+    send_data = {
         'text' : msg,
         'bot_id' : os.getenv('TARKSHARK_BOT_ID')
     }
-    #equest = requests.post("https://api.groupme.com/v3/bots/post", data = dumps(send_data))
-    request = Request(url, urlencode(data).encode())
-    json = urlopen(request).read().decode()
-    #print(request.text)
-    #print(request.url)
+    request = requests.post(send_url, params=send_data)
+    print(request.text)
+    print(request.url)
