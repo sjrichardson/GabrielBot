d24c646fbfdef9b3918f3050615f864f03d893a3
Samuel Richardson <samrichardson@sbcglobal.net>
2017-10-07 12:42:23 -0400

    this is python3

--- weather.py
+++ weather.py
@@ -15,8 +15,8 @@ def retrieve_weather(location):
 
     return return_string
 
-#converts direction from degrees to cardinal direction    
+#converts direction from degrees to cardinal direction
 def degToCompass(num):
     val=int((num/22.5)+.5)
     arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
-    print arr[(val % 16)]
+    print (arr[(val % 16)])
