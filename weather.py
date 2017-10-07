18b3391ec15ef1841a72ba0a8dc716de1961e7f9
Samuel Richardson <samrichardson@sbcglobal.net>
2017-10-07 13:35:08 -0400

    dsf

--- weather.py
+++ weather.py
@@ -28,4 +28,4 @@ def retrieve_weather(location):
 def degToCompass(num):
     val=int((num/22.5)+.5)
     arr=["N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"]
-    print (arr[(val % 16)])
+    return (arr[(val % 16)])
