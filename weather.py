e5dce772a73b8d5a3f152bbad119b6c3ae2c42b4
Samuel Richardson <samrichardson@sbcglobal.net>
2017-10-07 13:37:24 -0400

    ds

--- weather.py
+++ weather.py
@@ -2,27 +2,29 @@ import requests
 import json
 #pulls weather conditions from open weather map
 def retrieve_weather(location):
+    try:
+        location = location.replace('!weather ', '')
+        if len(location) == 0:
+            location = '47906'
+        payload = {
+            'zip' : '47906',
+            'units' : 'imperial',
+            'APPID' : 'f95d01fd26ff7a19e342a0621bd1fd93'
 
-    location = location.replace('!weather ', '')
-    if len(location) == 0:
-        location = '47906'
-    payload = {
-        'zip' : '47906',
-        'units' : 'imperial',
-        'APPID' : 'f95d01fd26ff7a19e342a0621bd1fd93'
+        }
+        weather = requests.get("http://api.openweathermap.org/data/2.5/weather", params=payload)
+        response = weather.json()
+        print(response)
+        temp = response['main']['temp']
+        wind = response['wind']['speed']
+        wind_direction = degToCompass(int(response['wind']['deg']))
 
-    }
-    weather = requests.get("http://api.openweathermap.org/data/2.5/weather", params=payload)
-    response = weather.json()
-    print(response)
-    temp = response['main']['temp']
-    wind = response['wind']['speed']
-    wind_direction = degToCompass(int(response['wind']['deg']))
+        sky = response['weather'][0]['main']
+        return_string = "Current conditions\n temp: {}, wind: {} {}, sky: {}".format(temp, wind, wind_direction, sky)
+        return return_string
+    except:
+        return "Cannot retrieve weather at this time"
 
-    sky = response['weather'][0]['main']
-    return_string = "Current conditions\n temp: {}, wind: {} {}, sky: {}".format(temp, wind, wind_direction, sky)
-
-    return return_string
 
 #converts direction from degrees to cardinal direction
 def degToCompass(num):
