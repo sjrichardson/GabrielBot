5a48cfe780d12b4c10a52c7d02d511c829b2e329
Sam <sam@dhcp-220-41.resnet.purdue.edu>
2018-03-28 01:40:27 -0400

    change quote style

--- bible.py
+++ bible.py
@@ -8,7 +8,8 @@ biblekey = os.getenv("BIBLE_KEY")
 #searches for given passage
 def bible_search(reference):
     payload = {
-        'q[]' : reference
+        "q[]" : reference,
+        "version" : "eng-ESV"
     }
 
     res = requests.get(os.getenv('BIBLE_URL'), auth=(biblekey, 'X'), params=payload)
